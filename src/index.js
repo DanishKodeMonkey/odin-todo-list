3708211c3b33bca7271b04d6e97703c07e74b595
danishkodemonkey <humberjini@hotmail.com>
2024-03-04 18:00:36 +0100

    Further cleanup

--- src/index.js
+++ src/index.js
@@ -8,7 +8,6 @@ import {
 	saveProjects,
 	getProjects,
 } from './modules/data-storage'
-import { ToDo } from './modules/todos'
 
 // Initialise page, checks for existing projects otherwise creates default project.
 // Also activates various modules
@@ -25,60 +24,8 @@ export default function init() {
 			getProjects()
 		} else {
 			let defaultProject = new project()
-
-			/* 			//TESTING AREA BELOW
-			let todoDefault = new ToDo(
-				'Default',
-				'todoDefault',
-				'todoDefaultdesc',
-				'2024-01-23',
-				false,
-				'todoDefaultNotes note'
-			)
-			defaultProject.addToProject(todoDefault)
-
-			let project1 = new project('project 1')
-			let ToDo1 = new ToDo(
-				'project 1',
-				'todo1',
-				'todo1desc',
-				'2024-01-03',
-				true,
-				'todo1Notes note'
-			)
-			let ToDo2 = new ToDo(
-				'project 1',
-				'todo2',
-				'todo2desc',
-				'2024-02-03',
-				true,
-				'todo2Notes note'
-			)
-			project1.addToProject(ToDo1)
-			project1.addToProject(ToDo2)
-
-			let project2 = new project('project 2')
-			let ToDo3 = new ToDo(
-				'project 2',
-				'todo3',
-				'todo3desc',
-				'2025-01-03',
-				true,
-				'todo3Notes note'
-			)
-			let ToDo4 = new ToDo(
-				'project 2',
-				'todo4',
-				'todo4desc',
-				'2024-10-03',
-				true,
-				'todo4Notes note'
-			)
-			project2.addToProject(ToDo3)
-			project2.addToProject(ToDo4)
-			// TEst end */
-
 			projectsArray.push(defaultProject)
+
 			console.log(`default object created, name: ${defaultProject.title}`)
 			console.log('saving to local storage...')
 			saveProjects(projectsArray)
